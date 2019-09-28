d55360508919a76cf4c974723f89cf7b41643bcd
610481663@qq,com <Pei5098252>
2019-09-29 00:48:29 +0800

    项目更新

--- Application/Common/Conf/database.php
+++ Application/Common/Conf/database.php
@@ -2,7 +2,7 @@
 return array(
 	'DB_TYPE'	=>  'mysql',
 	'DB_HOST'	=>  'localhost',
-	'DB_NAME'	=>  'ygbook',
+	'DB_NAME'	=>  'novel',
 	'DB_USER'	=>  'root',
 	'DB_PWD'	=>  '123456',
 	'DB_PORT'	=>  '3306',
